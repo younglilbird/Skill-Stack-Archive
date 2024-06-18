56f0cb82f0f3ea2e73f121e18e37e590d73bd4bb
Yulong Liang <91335818+younglilbird@users.noreply.github.com>
2024-06-18 12:14:39 -0400

    Update PayrollApplicationTests.java

--- src/test/java/com/example/demo/PayrollApplicationTests.java
+++ src/test/java/com/example/demo/PayrollApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PayrollApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testId() {
+		Employee employee = new Employee();
+		int id = employee.getId();
+		assertEquals(0, id);
 	}
 
 }
